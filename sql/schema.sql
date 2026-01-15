7a4d29a141d361b89b9cf1e12379c2ae17ce2354
mariojudah02 <mariuszfranzyk@gmail.com>
2026-01-15 15:03:42 +0100

    Initial project structure

---
+++ sql/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE raw_sales (
+    order_id        TEXT,
+    amount          NUMERIC,
+    profit          NUMERIC,
+    quantity        INTEGER,
+    category        TEXT,
+    sub_category    TEXT,
+    payment_mode    TEXT,
+    order_date      DATE,
+    customer_name   TEXT,
+    state           TEXT,
+    city            TEXT,
+    year_month      TEXT
+);
